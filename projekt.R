b20b24cc798de26bf032664ea8d43560a8f66dd9
Katja Marina <katja.marina@gmail.com>
2019-05-20 13:20:52 +0200

    auth.R

--- projekt.R
+++ projekt.R
@@ -20,12 +20,10 @@ delete_table <- function(){
     
     # Če tabela obstaja, jo zbrišemo, ter najprej zbrišemo tiste,
     # ki se navezujejo na druge
-    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS muzej CASCADE", con=conn))
-    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS prostor CASCADE", con=conn))
-    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS obdobje CASCADE", con=conn))
-    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS umetnik CASCADE", con=conn))
-    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS umetnina CASCADE", con=conn))
-    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS deloval CASCADE", con=conn))
+    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS donator CASCADE", con=conn))
+    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS prejemnik CASCADE", con=conn))
+    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS bolnisnica CASCADE", con=conn))
+    dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS kri CASCADE", con=conn))
     
   }, finally = {
     dbDisconnect(conn)
@@ -37,7 +35,7 @@ ustvari_tabele <- function(){
     conn <- dbConnect(drv, dbname = db, host = host,
                       user = user, password = password)
     #tabele
-    donor <- dbSendQuery(conn, build_sql("CREATE TABLE donor (
+    donator <- dbSendQuery(conn, build_sql("CREATE TABLE donator (
                                          id numeric PRIMARY KEY,
                                          ime text,
                                          kraj text,
@@ -46,7 +44,8 @@ ustvari_tabele <- function(){
                                          telefon numeric,
                                          teza numeric,
                                          hemoglobin numeric,
-                                         skupina text)", con=conn))
+                                         datum_donacije date,
+                                         krvna_skupina text)", con=conn))
     
     prejemnik <- dbSendQuery(conn, build_sql("CREATE TABLE prejemnik (
                                            id numeric PRIMARY KEY,
@@ -55,27 +54,35 @@ ustvari_tabele <- function(){
                                            drzava text,
                                            starost text,
                                            telefon text,
-                                           skupina text)", con=conn))
+                                           datum_vloge date,
+                                           krvna_skupina text)", con=conn))
     
     bolnisnica <- dbSendQuery(conn, build_sql("CREATE TABLE bolnisnica (
                                            id numeric PRIMARY KEY,
                                            ime text,
                                            kraj text,
                                            drzava text,
-                                           direktor text)", con=conn))
+                                           direktor text,
+                                           zaloga numeric)", con=conn))
     
     kri <- dbSendQuery(conn, build_sql("CREATE TABLE kri (
-                                            st_vrecke numeric PRIMARY KEY,
-                                            datum_prejetja date,
+                                            stevika_vrecke numeric PRIMARY KEY,
                                             hemoglobin numeric,
-                                            krvna_skupina text)", con=conn))
+                                            krvna_skupina text,
+                                            datum_prejetja date)", con=conn))
     
     #tabele relacij:
     
-    #deloval <- dbSendQuery(conn, build_sql("CREATE TABLE deloval(
-    #                                       umetnik_id TEXT NOT NULL REFERENCES umetnik(id),
-    #                                       obdobje_id TEXT NOT NULL REFERENCES obdobje(id))", con=conn))
+    #pomoje to ne rabva tabele: donira <- dbSendQuery(conn, build_sql("CREATE TABLE deloval(
+    #                                       donor_datum_prejetja TEXT NOT NULL REFERENCES donor(datum_donacije),
+    #                                       kri_datum_prejetja TEXT NOT NULL REFERENCES kri(datum_prejetja))", con=conn))
     
+    #to tut vrjetn ne rabva?: prejme <- dbSendQuery(conn, build_sql("CREATE TABLE hrani(
+    #                                        donor_datum_prejetja TEXT NOT NULL REFERENCES donor(datum_donacije),
+    #                                       kri_datum_prejetja TEXT NOT NULL REFERENCES kri(datum_prejetja))", con=conn))
+    #hrani <-
+    
+    #nahaja <-
     
   }, finally = {
     # Na koncu nujno prekinemo povezavo z bazo,
@@ -88,17 +95,17 @@ ustvari_tabele <- function(){
 }
 
 #Funcija, ki vstavi podatke
-#insert_data <- function(){
-#  tryCatch({
-#    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
-#    
-#    dbWriteTable(conn, name="muzej", muzej, append=T, row.names=FALSE)
-#    
-#  }, finally = {
-#    dbDisconnect(conn) 
-#    
-#  })
-#}
+insert_data <- function(){
+  tryCatch({
+    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+    
+    dbWriteTable(conn, name="donator", donator, append=T, row.names=FALSE)
+    
+  }, finally = {
+    dbDisconnect(conn) 
+    
+  })
+}
 
 pravice <- function(){
   # Uporabimo tryCatch,(da se povežemo in bazo in odvežemo)
@@ -122,4 +129,4 @@ pravice <- function(){
 pravice()
 delete_table()
 ustvari_tabele()
-#insert_data()
+insert_data()
